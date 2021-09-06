dab07abc8cd9f465d110b73a659f0ee7b387a6c3
Natan <natannjos@gmail.com>
2021-09-06 10:06:36 -0300

    Add gesture handler and print a message

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(title: 'Tutorial de Flutter', home: TutorialHome()));
-}
-
-class TutorialHome extends StatelessWidget {
-  const TutorialHome({Key? key}) : super(key: key);
+class MyButton extends StatelessWidget {
+  const MyButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Scaffold é um layout para
-    // a maioria dos Componentes Material
-    return Scaffold(
-      appBar: AppBar(
-        leading: const IconButton(
-          onPressed: null,
-          icon: Icon(Icons.menu),
-          tooltip: 'Menu de Navegação',
+    return GestureDetector(
+      onTap: () {
+        print('O botão foi clicado!');
+      },
+      child: Container(
+        height: 50.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5.0),
+            color: Colors.lightGreen[500]),
+        child: const Center(
+          child: Text('Engage'),
         ),
-        title: Text('Título de Exemplo'),
-        actions: const [
-          IconButton(
-            onPressed: null,
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-          )
-        ],
-      ),
-      body: const Center(
-        child: Text('Texto do Contador'),
-      ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.add),
-        tooltip: 'Add',
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: MyButton(),
+      ),
+    ),
+  ));
 }
