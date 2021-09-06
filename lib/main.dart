9bea900ec483ef352fcd728d621cbed9a9b32e53
Natan <natannjos@gmail.com>
2021-09-06 11:08:19 -0300

    Separate concerns of each widget individualy

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
 
+class CounterIncrementor extends StatelessWidget {
+  const CounterIncrementor({required this.onPressed, Key? key})
+      : super(key: key);
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPressed, child: const Text('Add'));
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  const CounterDisplay({required this.counter, Key? key}) : super(key: key);
+  final int counter;
+  @override
+  Widget build(BuildContext contextn) {
+    return Text('Contador: $counter');
+  }
+}
+
 class Counter extends StatefulWidget {
   const Counter({Key? key}) : super(key: key);
   @override
@@ -22,11 +43,13 @@ class _CounterState extends State<Counter> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        ElevatedButton(onPressed: _increment, child: const Text('Add')),
+        CounterIncrementor(onPressed: _increment),
         const SizedBox(
           width: 16,
         ),
-        Text('Contador: $_counter')
+        CounterDisplay(
+          counter: _counter,
+        )
       ],
     );
   }
